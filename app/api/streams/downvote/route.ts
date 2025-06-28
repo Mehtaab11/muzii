d1c58e44bc8db64ce7538dc0ccf20d2af1fe7e64
Mehtaab <mehtaabaalam117@gmail.com>
2025-06-28 17:55:50 +0530

    downvote route

--- app/api/streams/downvote/route.ts
+++ app/api/streams/downvote/route.ts
@@ -1,45 +1,51 @@
 import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-const YT_REGEX = new RegExp(
-  /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:watch\?(?!.*\blist=)(?:.*&)?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&]\S+)?$/
-);
 
-const StreamSchema = z.object({
-  creatorId: z.string(),
-  url: z.string(),
+const UpvoteSchema = z.object({
+  streamId: z.string(),
 });
 
 export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email || "",
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: "User not found",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+
   try {
-    const data = StreamSchema.parse(await req.json());
+    const data = UpvoteSchema.parse(await req.json());
 
-    const isYt = YT_REGEX.test(data.url);
-    if (!isYt) {
-      return NextResponse.json(
-        {
-          message: "Invalid Youtube URL",
+    await prismaClient.upvote.delete({
+      where: {
+        userId_streamId: {
+          userId: user.id,
+          streamId: data.streamId,
         },
-        { status: 411 }
-      );
-    }
-
-    const extractedId = data.url.split("v=")[1];
-
-    await prismaClient.stream.create({
-      data: {
-        userId: data.creatorId,
-        url: data.url,
-        extractedId,
-        type: "Youtube",
       },
     });
   } catch (error) {
     return NextResponse.json(
       {
-        message: "Error while creating stream",
+        message: "Error while upvoting stream",
       },
-      { status: 411 }
+      {
+        status: 500,
+      }
     );
   }
 }
