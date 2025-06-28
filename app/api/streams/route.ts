e70c650d84edc8aa7d7314c947186e3c346852e7
Mehtaab <mehtaabaalam117@gmail.com>
2025-06-28 17:53:34 +0530

    feat(database): initialize schema with User, Stream, and Upvote tables
    - Created enum type "Provider" for user authentication.
    - Added "User" table with fields: id, email, and provider.
    - Added "Stream" table with fields: id, type, active, userId.
    - Added "Upvote" table with fields: id, userId, streamId.
    - Established foreign key relationships between tables.
    - Created unique index on Upvote for userId and streamId.
    
    feat(database): add extractedId and url columns to Stream table
    
    - Added required columns "extractedId" and "url" to the "Stream" table.
    - Note: Adding these columns without default values may cause issues if the table is not empty.
    
    chore(migrations): add migration lock file for version control
    
    - Added migration_lock.toml to track migration state.

---
+++ app/api/streams/route.ts
@@ -0,0 +1,45 @@
+import { prismaClient } from "@/app/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+const YT_REGEX = new RegExp(
+  /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:watch\?(?!.*\blist=)(?:.*&)?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&]\S+)?$/
+);
+
+const StreamSchema = z.object({
+  creatorId: z.string(),
+  url: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = StreamSchema.parse(await req.json());
+
+    const isYt = YT_REGEX.test(data.url);
+    if (!isYt) {
+      return NextResponse.json(
+        {
+          message: "Invalid Youtube URL",
+        },
+        { status: 411 }
+      );
+    }
+
+    const extractedId = data.url.split("v=")[1];
+
+    await prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId,
+        type: "Youtube",
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error while creating stream",
+      },
+      { status: 411 }
+    );
+  }
+}
